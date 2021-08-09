7e025678bad142c132290784271570b491aa5c86
Cesar Valle <cevpino@gmail.com>
2021-08-08 19:59:10 -0600

    Se agrega Login, Register, NotFound y el componente Layout, se modifica el Home para que no repita el Header y el footer que se han agregado al Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import '../assets/styles/App.scss';
 
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
@@ -15,8 +13,7 @@ const API = 'http://localhost:3000/initialState';
 const Home = () => {
     const initialState = useInitialState(API);
     return initialState.length === 0 ? <h1>Loading...</h1> : (
-        <div className='Home'>
-            <Header />
+        <>
             <Search />
             {initialState.mylist !==undefined && initialState.mylist?.length > 0 && (
                 <Categories title="Mi lista">
@@ -41,8 +38,7 @@ const Home = () => {
                     )}
                 </Carousel>
             </Categories>
-            <Footer />
-        </div>
+        </>
     );
 }
 
---
+++ src/containers/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../assets/styles/components/Login.scss';
+import googleIcon from '../assets/static/img/google_icon.png';
+import twitterIcon from '../assets/static/img/twitter_icon.png';
+
+const Login = () => (
+    <section className='login'>
+		<section className='login__container'>
+			<h2>Inicia sesión</h2>
+			<form className='login__container--form'>
+				<input className='input' type='text' placeholder='Correo' />
+				<input className='input' type='password' placeholder='Contraseña' />
+				<button className='button'>Iniciar sesión</button>
+				<div className='login__container--remember-me'>
+					<label>
+						<input type='checkbox' id='cbox1' value='first_checkbox' />
+						Recuérdame
+					</label>
+					<a href='/'>Olvidé mi contraseña</a>
+				</div>
+			</form>
+			<section className='login__container--social-media'>
+				<div>
+					<img src={googleIcon} alt="Icono de Google" /> Inicia sesión con Google
+				</div>
+				<div>
+					<img src={twitterIcon} alt="Twitter" /> Inicia sesión con Twitter
+				</div>
+			</section>
+			<p className='login__container--register'>
+				No tienes ninguna cuenta <a href=''>Regístrate</a>
+			</p>
+		</section>
+	</section>
+);
+
+export default Login;
